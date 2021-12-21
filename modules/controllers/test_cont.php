6cc7ff12acaeede65958ee8dedb4a1196c2552ac
Dimik151 <dmitry_zvorigin@mail.ru>
2021-12-21 16:11:58 +0500

    'main'

--- modules/controllers/test_cont.php
+++ modules/controllers/test_cont.php
@@ -9,7 +9,7 @@ class Test_Cont extends BaseController
 {
     public function test()
     {
-        $ctx = ['test' => new \Models\Test_Mod()];
+        $ctx = ['test' => new \Models\Test_Mod(), 'site_title' => 'TEST'];
         $this->render('test', $ctx);
     }
 }
--- modules/settings.php
+++ modules/settings.php
@@ -1,2 +1,6 @@
 <?php 
 // Файл с настройками сайта
+namespace Settings 
+{
+    const SITE_NAME = 'TEST';
+}
